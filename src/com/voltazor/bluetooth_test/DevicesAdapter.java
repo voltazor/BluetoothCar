40d4e8581064a0fdcdbf3579effcbab250269d4e
Dmitriy Dovbnya <voltazor@gmail.com>
2014-01-22 07:28:29 +0200

    some code

--- src/com/voltazor/bluetooth_test/DevicesAdapter.java
+++ src/com/voltazor/bluetooth_test/DevicesAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,30 +25,48 @@ public class DevicesAdapter extends BaseAdapter {
         this.devices = devices;
     }
 
+    public synchronized void updateContent(List<BluetoothDevice> devices) {
+        if (devices == null) {
+            devices = new ArrayList<BluetoothDevice>();
+        }
+        this.devices = devices;
+        notifyDataSetChanged();
+    }
+
     @Override
-    public Object getItem(int position) {
+    public BluetoothDevice getItem(int position) {
         return devices.get(position);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
+        BluetoothDevice item;
 
         if (convertView == null) {
             convertView = LayoutInflater.from(context).inflate(R.layout.devices_list_item, null);
 
+            holder = new ViewHolder();
+            holder.name = (TextView) convertView.findViewById(R.id.name);
+            holder.address = (TextView) convertView.findViewById(R.id.address);
 
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
 
+        if ((item = getItem(position)) != null) {
+            holder.name.setText(item.getName());
+            holder.address.setText(item.getAddress());
+        }
 
-        
-        return null;
+        return convertView;
     }
 
     @Override
     public int getCount() {
-        return devices.size();
+        return devices != null ? devices.size() : 0;
     }
 
     @Override
@@ -56,10 +75,8 @@ public class DevicesAdapter extends BaseAdapter {
     }
 
     private class ViewHolder {
-
         TextView name;
         TextView address;
-
     }
 
 }
--- src/com/voltazor/bluetooth_test/MainActivity.java
+++ src/com/voltazor/bluetooth_test/MainActivity.java
@@ -2,30 +2,45 @@ package com.voltazor.bluetooth_test;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.ToggleButton;
+import android.view.View;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     private BluetoothAdapter mBluetoothAdapter;
 
-    private ToggleButton toggleState;
-    private Button startSearching;
-    private ListView foundDevices;
+    private DevicesAdapter adapter;
+
+    private List<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+
+    private boolean registered = false;
+    private boolean connected = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        toggleState = (ToggleButton) findViewById(R.id.toggleState);
-        startSearching = (Button) findViewById(R.id.startSearching);
-        foundDevices = (ListView) findViewById(R.id.foundDevices);
-
         if (checkBT()) {
             initUI();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (registered) {
+            unregisterReceiver(mReceiver);
         }
     }
 
@@ -35,13 +50,89 @@ public class MainActivity extends Activity {
     }
 
     private void initUI() {
-        toggleState = (ToggleButton) findViewById(R.id.toggleState);
-        startSearching = (Button) findViewById(R.id.startSearching);
-        foundDevices = (ListView) findViewById(R.id.foundDevices);
+        ToggleButton toggleState = (ToggleButton) findViewById(R.id.toggleState);
+        final Button startSearching = (Button) findViewById(R.id.startSearching);
+        ListView foundDevices = (ListView) findViewById(R.id.foundDevices);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress);
 
+        toggleState.setEnabled(true);
         toggleState.setChecked(mBluetoothAdapter.isEnabled());
+        toggleState.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                setBluetoothEnabled(isChecked);
+                startSearching.setEnabled(isChecked);
+            }
+        });
+
+        startSearching.setEnabled(toggleState.isChecked());
+        startSearching.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, 10000);
+                devices.clear();
+                adapter.updateContent(devices);
+                startSearching();
+            }
+        });
+
+        adapter = new DevicesAdapter(this, devices);
+        foundDevices.setAdapter(adapter);
+        foundDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                connectToDevice(position);
+            }
+        });
+    }
+
+    private void setBluetoothEnabled(boolean enabled) {
+        if (enabled) {
+            mBluetoothAdapter.enable();
+        } else {
+            mBluetoothAdapter.disable();
+        }
+    }
+
+    private void startSearching() {
+        mBluetoothAdapter.cancelDiscovery();
+        mBluetoothAdapter.startDiscovery();
+        registerReceiver(mReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        registered = true;
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView
+                if (!devices.contains(device)) {
+                    devices.add(device);
+                    adapter.updateContent(devices);
+                }
+            }
+        }
+    };
 
 
+    private void connectToDevice(int position) {
+        if (connected) {
+            ConnectedThread.cancel();
+        } else {
+            BluetoothDevice device = devices.get(position);
+            (new ConnectThread(device)).start();
+            connected = true;
+        }
 
     }
 
